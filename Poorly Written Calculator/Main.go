0f96b5ed78d61cb88a184de837dac039d7ef4960
DemiDemiDocker <deepdaddydemi@gmail.com>
2025-08-13 22:14:36 +0800

    Added some better option ui

--- Poorly Written Calculator/Main.go
+++ Poorly Written Calculator/Main.go
@@ -70,13 +70,34 @@ func main() {
 	)
 	window.SetContent(content)
 
-	toolsMenu := fyne.NewMenu("Options",
-		fyne.NewMenuItem("Light Mode", func() { application.Settings().SetTheme(theme.LightTheme()) }),
-		fyne.NewMenuItem("Dark Mode", func() { application.Settings().SetTheme(theme.DarkTheme()) }),
-		fyne.NewMenuItem("Made by Demi", nil),
+	var toolsMenu *fyne.Menu
+	var aboutMenu *fyne.Menu
+	var toggleItem *fyne.MenuItem
+	currentVariant := application.Settings().ThemeVariant()
+	isDark := currentVariant == theme.VariantDark
+	initialLabel := "Dark Mode"
+	if isDark {
+		initialLabel = "Light Mode"
+	}
+	toggleItem = fyne.NewMenuItem(initialLabel, func() {
+		if isDark {
+			application.Settings().SetTheme(theme.LightTheme())
+			toggleItem.Label = "Dark Mode"
+			isDark = false
+		} else {
+			application.Settings().SetTheme(theme.DarkTheme())
+			toggleItem.Label = "Light Mode"
+			isDark = true
+		}
+		window.SetMainMenu(fyne.NewMainMenu(toolsMenu, aboutMenu))
+	})
+
+	toolsMenu = fyne.NewMenu("Options",
+		toggleItem,
 	)
 
-	window.SetMainMenu(fyne.NewMainMenu(toolsMenu))
+	aboutMenu = fyne.NewMenu("Made by Demi")
 
+	window.SetMainMenu(fyne.NewMainMenu(toolsMenu, aboutMenu))
 	window.ShowAndRun()
 }
